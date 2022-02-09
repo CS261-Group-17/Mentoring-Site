d9bd8183e8c0756359497e113e423f154deb3379
PlebeianCoder <benjlewis2002@gmail.com>
2022-02-09 20:59:16 +0000

    Basic changes

---
+++ mentorprise/frontend_dev/src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+    <img alt="MentorPrise logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Login",
+  components: {
+  },
+};
+</script>
