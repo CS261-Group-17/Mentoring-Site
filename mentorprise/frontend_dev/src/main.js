d395245ff3d170686d03f7eac29fabbdcc70bb39
PlebeianCoder <benjlewis2002@gmail.com>
2022-02-11 10:20:45 +0000

    Installed bootstrap properly

--- mentorprise/frontend_dev/src/main.js
+++ mentorprise/frontend_dev/src/main.js
@@ -1,8 +1,11 @@
-import { createApp } from "vue";
+import { createApp, Vue } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import BootstrapVue from "bootstrap-vue"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 createApp(App).use(router).mount("#app");
