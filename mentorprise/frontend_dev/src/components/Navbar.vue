86f526e65870890eb716e416623e395fb3118c80
PlebeianCoder <benjlewis2002@gmail.com>
2022-02-11 11:05:16 +0000

    Temporary Save

---
+++ mentorprise/frontend_dev/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="navbar">
+        <p>Hi</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Navbar",
+    props: {}
+}
+</script>
+
+<style scoped>
+    #navbar {
+        color: whitesmoke;
+        background-color: #0A102C;
+    }
+</style>
---
+++ mentorprise/frontend_dev/src/views/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+    <Navbar />
+</template>
+
+<script>
+    import Navbar from "../components/Navbar.vue";
+
+    export default {
+        name: "Profile",
+        props: {},
+        components: {
+            Navbar
+        }
+    }
+</script>
