69b6779198c2d6c96cb40a4bde5892710188a983
PlebeianCoder <77303709+PlebeianCoder@users.noreply.github.com>
2022-02-11 10:44:39 +0000

    Temporarily fix to main.js

--- mentorprise/frontend_dev/src/main.js
+++ mentorprise/frontend_dev/src/main.js
@@ -1,11 +1,5 @@
-import { createApp, Vue } from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
-import BootstrapVue from "bootstrap-vue"
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
 
 createApp(App).use(router).mount("#app");
