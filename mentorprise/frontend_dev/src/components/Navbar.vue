0c1e4c610c7f59b50bf9f13b299565ea0282d200
PlebeianCoder <benjlewis2002@gmail.com>
2022-02-11 15:51:52 +0000

    Navbar

--- mentorprise/frontend_dev/src/components/Navbar.vue
+++ mentorprise/frontend_dev/src/components/Navbar.vue
@@ -1,11 +1,19 @@
 <template>
     <div id="navbar">
-        <p>Hi</p>
+        <img id="logo" src="../assets/logo.png" alt="Logo">
+        <span>
+            <ul>
+                <li class="nav-item"><router-link class="nav-link" to="/Dashboard">Dashboard</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" to="/POA">Plans Of Action</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" to="/Schedule">Schedule</router-link></li>
+                <li class="nav-item"><router-link class="nav-link" to="/GroupEvents">Group Events</router-link></li>
+            </ul>
+        </span>
+        
     </div>
 </template>
 
 <script>
-
 export default {
     name: "Navbar",
     props: {}
@@ -16,5 +24,34 @@ export default {
     #navbar {
         color: whitesmoke;
         background-color: #0A102C;
+    }
+    #logo {
+        width: 7rem;
+        float: left;
+        display: inline;
+    }
+    .nav-link {
+        color: whitesmoke;
+    }
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        display: block;
+        width: auto;
+    }
+    li {
+        float: left;
+    }
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 2rem 1rem;
+        text-decoration: none;
+    }
+    li a:hover {
+        color: grey;
     }
 </style>
