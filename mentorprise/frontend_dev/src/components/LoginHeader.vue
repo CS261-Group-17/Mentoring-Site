052de060fa6419fae439dd2acb458d3e8d316115
Jayre123 <jayre.ng@gmail.com>
2022-02-21 10:50:44 +0000

    Login.vue with components

---
+++ mentorprise/frontend_dev/src/components/LoginHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
