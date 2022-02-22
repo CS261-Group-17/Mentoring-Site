962b0f8c52f4dc3e27b8955772b5373685e4b459
PlebeianCoder <benjlewis2002@gmail.com>
2022-02-22 17:45:12 +0000

    Half done schedule

---
+++ mentorprise/frontend_dev/src/components/Calendar.vue
@@ -0,0 +1,135 @@
+<template>
+    <div id="calendar">
+        <ul> 
+            <li v-for="dayEvent in intoDays(calendar)" :key="dayEvent[0].date" class="dayEvents">
+                <h3>{{dateToString(dayEvent[0].date)}}</h3>
+                <ul>
+                    <li v-for="event in dayEvent" :key="event.eventID" :class="'events'+event.type">
+                        <p>
+                            {{event.eventName}}<br>
+                            {{event.startTime + " - " + event.endTime + ", " + event.location}}
+                        </p>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Calendar",
+        props: {
+            events: Array
+        },
+        data() {
+            return {
+                calender: []
+            }
+        },
+        components: {},
+        methods: {
+             sortByDate(currentEvents) {
+                if(currentEvents.length < 2) {
+                    //alert(currentEvents.length)
+                    return currentEvents;
+                }
+                let mid = Math.ceil(currentEvents.length / 2);
+                let arr1 = this.sortByDate(currentEvents.slice(0, mid));
+                let arr2 = this.sortByDate(currentEvents.slice(mid, currentEvents.length));
+                let newArr = [], p1 = 0, p2 = 0;
+                while(newArr.length < currentEvents.length) {
+                    if(p1 >= arr1.length) {
+                        // finished with first list
+                        newArr.push(arr2[p2]);
+                        p2+=1;
+                    }
+                    else if (p2 >= arr2.length) {
+                        // finished with second list
+                        newArr.push(arr1[p1]);
+                        p1+=1;
+                    }
+                    else if(Date.parse(arr1[p1].date + "T" + arr1[p1].startTime) >= Date.parse(arr2[p2].date + "T" + arr2[p2].startTime)) {
+                        // second array is earlier
+                        newArr.push(arr2[p2]);
+                        p2+=1;
+                    }
+                    else {
+                        // first array is earlier
+                        newArr.push(arr1[p1]);
+                        p1+=1;
+                    }
+                }
+                return newArr
+            },
+            sortEventsByDate() {
+                //alert("HI");
+                return this.sortByDate(this.events)
+            },
+            intoDays(calendar) {
+                if(calendar.length == 0) {
+                    return calendar
+                }
+                let daysCalendar = [[calendar[0]]]
+                let currentDay = calendar[0].date
+                for(let i=1;i<calendar.length;i++) {
+                    if(currentDay == calendar[i].date) {
+                        daysCalendar[daysCalendar.length-1].push(calendar[i])
+                    }
+                    else {
+                        currentDay = calendar[i].date
+                        daysCalendar.push([calendar[i]])
+                    }
+                }
+                return daysCalendar
+            },
+            dateToString(ourDate) {
+                let dateDate = new Date(ourDate);
+                let today = new Date();
+                let tomorrow = new Date();
+                tomorrow.setDate(today.getDate() + 1);
+                if(today.getDate() == dateDate.getDate()) {
+                    return "Today"
+                }
+                if(tomorrow.getDate() == dateDate.getDate()) {
+                    return "Tomorrow"
+                }
+                return dateDate.toString().substring(4, 15)
+            }
+        },
+        created() {
+            this.calendar = this.sortEventsByDate();
+        }
+    }
+</script>
+
+<style scoped>
+    #calendar {
+        background-color: #0A102C;
+        padding: 1rem;
+        float: left;
+        width: 40%;
+    }
+    .eventsm, .eventsg, .eventsw {
+        list-style-type: none;
+        padding-left: 0.5rem;
+    }
+    .eventsm {
+        background-color: #F9B59C;
+        border-left: 8px solid #F15A24;
+    }
+    .eventsg {
+        background-color: #9196FF;
+        border-left: 8px solid #101AFF;
+    }
+    .eventsw {
+        background-color: #F798C1;
+        border-left: 8px solid #ED1E79;
+    }
+    .dayEvents {
+        list-style-type: none;
+    }
+    ul {
+        padding: 0rem;
+    }
+</style>
--- mentorprise/frontend_dev/src/views/Schedule.vue
+++ mentorprise/frontend_dev/src/views/Schedule.vue
@@ -1,14 +1,137 @@
 <template>
-  <div class="schedule">
-    <p>One day this will grow into a full schedule page</p>
-    <router-link to="/Dashboard">Dashboard</router-link>
-  </div>
+    <NavBar />
+    <div id="schedule">
+        <h1>Schedule</h1>
+        <Calendar :events="this.schedule"/>
+    </div>
+
 </template>
 
 <script>
-export default {
-  name: "Schedule",
-  components: {
-  },
-};
+    import NavBar from "../components/Navbar.vue"
+    import Calendar from "../components/Calendar.vue"
+
+    export default {
+    name: "Schedule",
+    components: {
+        NavBar,
+        Calendar
+    },
+    data() {
+        return {
+            profile: {},
+            requests: [],
+            schedule: []
+        }
+    },
+    created() {
+        this.profile = {
+                first: "Ben",
+                last:  "Lewis",
+                email: "u2003284@warwick.live.ac.uk",
+                jobTitle: "Student",
+                isMentor: true,
+                businessArea_type: "dev",
+                password: "CompSci",
+                ss: [{
+                        id: 1,
+                        val: "tennis",
+                    },
+                    {
+                        id: 2,
+                        val: "team"
+                    }
+                ],
+                ws: [{
+                        id: 1,
+                        val: "communication"
+                    },
+                    {
+                        id: 2,
+                        val: "friendly"   
+                    }                
+                ]
+        },
+        this.requests = [
+            // Request types:
+            // . Feedback on event (FE)
+            // . Meeting requests (M)
+            // . Feedback on mentor/mentee (FM)
+            {
+                requestID: 1,
+                requestType:"FE",
+                eventID: 2,
+                eventName: "Catchup"
+            },
+            {
+                requestID: 2,
+                requestType: "FM",
+                mentorID: 4,
+                mentorName: "Bill"
+            },
+            {
+                requestID: 3,
+                requestType: "M",
+                requesteeID: 7,
+                requesteeName: "Mary"
+            }
+        ]
+        this.schedule = [
+            // Date in YYYY-MM-DD
+            // type: w, m, g
+            {
+                eventID: 1,
+                eventName: "Meeting with Bill",
+                date: "2022 02 22",
+                startTime: "11:00",
+                endTime: "13:00",
+                location: "CS35.1",
+                type: "m"
+            },
+            {
+                eventID: 2,
+                eventName: "Meeting with Harry",
+                date: "2022 02 22",
+                startTime: "16:00",
+                endTime: "17:00",
+                location: "CS36.2",
+                type: "m"
+            },
+            {
+                eventID: 3,
+                eventName: "Python Workshop for Beginners",
+                date: "2022 02 23",
+                startTime: "10:00",
+                endTime: "12:00",
+                location: "T35",
+                type: "w"
+            },
+            {
+                eventID: 4,
+                eventName: "Group Session - Mentoring Support",
+                date: "2022 02 24",
+                startTime: "14:00",
+                endTime: "15:00",
+                location: "MS Teams",
+                type: "g"
+            },
+            {
+                eventID: 5,
+                eventName: "Meeting with Harry",
+                date: "2022 02 28",
+                startTime: "16:00",
+                endTime: "17:00",
+                location: "CS36.2",
+                type: "m"
+            },
+        ]
+    }
+    };
 </script>
+
+<style scoped>
+    #schedule {
+        padding: 2rem;
+        color: white;
+    }
+</style>
