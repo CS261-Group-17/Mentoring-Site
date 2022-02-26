67b484c14cc8cb57e8f23e0a54b65a99dc5ec26d
EdmundGoodman <egoodman3141@gmail.com>
2022-02-26 23:00:15 +0000

    Added strengths/weaknesses endpoint

--- mentorprise/backend_dev/mentorprise/app/serializers.py
+++ mentorprise/backend_dev/mentorprise/app/serializers.py
@@ -6,3 +6,8 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['first_name', 'last_name', 'biography', 'email', 'business_area', 'job_title', 'expert_at', 'mentor']
         # fields = model.__dict__["__doc__"].split("(")[1][:-1].split(", ") # Get all member variables
+
+class StrengthWeaknessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StrengthWeakness
+        fields = ['sw_type']
--- mentorprise/backend_dev/mentorprise/app/urls.py
+++ mentorprise/backend_dev/mentorprise/app/urls.py
@@ -3,12 +3,16 @@ from app import views
 
 urlpatterns = [
     path('profiles/',
-         views.user_list,
+         views.user_profile_list,
          name = 'user-list'),
     path('register/',
          views.register,
          name = 'user-register'),
     path('profiles/<str:email>/',
-         views.get_profile,
+         views.user_profile,
          name = 'user-detail'),
+
+    path('strengths_weaknesses/',
+         views.strength_weaknesses,
+         name = 'strength-weaknesses-list'),
 ]
