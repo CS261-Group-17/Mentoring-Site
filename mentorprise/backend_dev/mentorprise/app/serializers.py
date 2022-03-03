fe25a443c7a8db500c2254d5da8884e8a765f470
Tomás Chapman Fromm <tomas@chapmanfromm.com>
2022-03-03 18:34:10 +0000

    change class ordering

--- mentorprise/backend_dev/mentorprise/app/serializers.py
+++ mentorprise/backend_dev/mentorprise/app/serializers.py
@@ -2,6 +2,17 @@ from dataclasses import field
 from rest_framework import serializers
 from app.models import *
 
+class StrengthWeaknessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StrengthWeakness
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    expert_at = StrengthWeaknessSerializer()
+    class Meta:
+        model = Profile
+        fields = ['biography', 'business_area', 'job_title', 'expert_at', 'mentor']
+
 class UserSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer()
 
@@ -14,14 +25,3 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create(**validated_data)
         Profile.objects.create(user=user, **profile_data)
         return user
-
-class ProfileSerializer(serializers.ModelSerializer):
-    expert_at = StrengthWeaknessSerializer()
-    class Meta:
-        model = Profile
-        fields = ['biography', 'business_area', 'job_title', 'expert_at', 'mentor']
-
-class StrengthWeaknessSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StrengthWeakness
-        fields = '__all__'
