9cb0551429a1027de14ef6ea3ca900e6ab28e973
Tomás Chapman Fromm <tomas@chapmanfromm.com>
2022-03-04 14:22:54 +0000

    user serializers done

--- mentorprise/backend_dev/mentorprise/app/serializers.py
+++ mentorprise/backend_dev/mentorprise/app/serializers.py
@@ -8,10 +8,9 @@ class StrengthWeaknessSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProfileSerializer(serializers.ModelSerializer):
-    expert_at = StrengthWeaknessSerializer()
     class Meta:
         model = Profile
-        fields = ['biography', 'business_area', 'job_title', 'expert_at', 'mentor']
+        exclude = ['user']
 
 class UserSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer()
@@ -25,3 +24,33 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create(**validated_data)
         Profile.objects.create(user=user, **profile_data)
         return user
+
+# class NotificationSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Notification
+#         fields = '__all__'
+
+# class MeetingSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Meeting
+#         fields = '__all__'
+
+# class GroupEventWeaknessesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = GroupEventWeaknesses
+#         fields = ['weakness_type']
+
+# class GroupEventSerializer(serializers.ModelSerializer):
+#     weaknesses = GroupEventWeaknessesSerializer(many=True)
+
+#     class Meta:
+#         model = GroupEvent
+#         fields = ['instructor', 'title', 'description', 'start_datetime', 'duration', 'cancelled', 'event_type', 'capacity', 'weaknesses']
+
+#     def create(self, validated_data):
+#         weakness_data = validated_data.pop('weaknesses')
+#         group_event = GroupEvent.objects.create(**validated_data)
+#         weakness_list = [GroupEventWeaknesses(weakness_type=item, event=group_event) for item in weakness_data]
+#         GroupEventWeaknesses.objects.bulk_create(weakness_list)
+#         return group_event
+             
