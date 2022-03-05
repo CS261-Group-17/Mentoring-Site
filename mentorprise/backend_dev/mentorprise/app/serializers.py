0fe3ff1a53c5f0f22d70536aee5b4de64e97ebbd
EdmundGoodman <egoodman3141@gmail.com>
2022-03-05 19:57:51 +0000

    Added support for user registration

--- mentorprise/backend_dev/mentorprise/app/serializers.py
+++ mentorprise/backend_dev/mentorprise/app/serializers.py
@@ -24,105 +24,105 @@ class UserSerializer(serializers.ModelSerializer):
         Profile.objects.create(user=user, **profile_data)
         return user
 
-class MeetingWeaknessesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MeetingWeaknesses
-        fields = ['weakness_type']
+# class MeetingWeaknessesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MeetingWeaknesses
+#         fields = ['weakness_type']
 
-class MeetingSerializer(serializers.ModelSerializer):
-    weaknesses = MeetingWeaknessesSerializer(many=True)
+# class MeetingSerializer(serializers.ModelSerializer):
+#     weaknesses = MeetingWeaknessesSerializer(many=True)
 
-    class Meta:
-        model = Meeting
-        fields = ['instructor', 'title', 'description', 'start_datetime', 'duration', 'cancelled', 'mentee', 'weaknesses']
+#     class Meta:
+#         model = Meeting
+#         fields = ['instructor', 'title', 'description', 'start_datetime', 'duration', 'cancelled', 'mentee', 'weaknesses']
 
-    def create(self, validated_data):
-        weakness_data = validated_data.pop('weaknesses')
-        meeting = Meeting.objects.create(**validated_data)
-        weakness_list = [MeetingWeaknesses(weakness_type=item['weakness_type'], event=meeting) for item in weakness_data]
-        MeetingWeaknesses.objects.bulk_create(weakness_list)
-        return meeting
-             
+#     def create(self, validated_data):
+#         weakness_data = validated_data.pop('weaknesses')
+#         meeting = Meeting.objects.create(**validated_data)
+#         weakness_list = [MeetingWeaknesses(weakness_type=item['weakness_type'], event=meeting) for item in weakness_data]
+#         MeetingWeaknesses.objects.bulk_create(weakness_list)
+#         return meeting
 
-class GroupEventWeaknessesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GroupEventWeaknesses
-        fields = ['weakness_type']
 
-class GroupEventSerializer(serializers.ModelSerializer):
-    weaknesses = GroupEventWeaknessesSerializer(many=True)
+# class GroupEventWeaknessesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = GroupEventWeaknesses
+#         fields = ['weakness_type']
 
-    class Meta:
-        model = GroupEvent
-        fields = ['instructor', 'title', 'description', 'start_datetime', 'duration', 'cancelled', 'event_type', 'capacity', 'weaknesses']
+# class GroupEventSerializer(serializers.ModelSerializer):
+#     weaknesses = GroupEventWeaknessesSerializer(many=True)
 
-    def create(self, validated_data):
-        weakness_data = validated_data.pop('weaknesses')
-        group_event = GroupEvent.objects.create(**validated_data)
-        weakness_list = [GroupEventWeaknesses(weakness_type=item['weakness_type'], event=group_event) for item in weakness_data]
-        GroupEventWeaknesses.objects.bulk_create(weakness_list)
-        return group_event
-             
-class AttendanceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Attendance
-        fields = '__all__'
+#     class Meta:
+#         model = GroupEvent
+#         fields = ['instructor', 'title', 'description', 'start_datetime', 'duration', 'cancelled', 'event_type', 'capacity', 'weaknesses']
 
-class MeetingFeedbackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MeetingFeedback
-        fields = '__all__'
+#     def create(self, validated_data):
+#         weakness_data = validated_data.pop('weaknesses')
+#         group_event = GroupEvent.objects.create(**validated_data)
+#         weakness_list = [GroupEventWeaknesses(weakness_type=item['weakness_type'], event=group_event) for item in weakness_data]
+#         GroupEventWeaknesses.objects.bulk_create(weakness_list)
+#         return group_event
 
-class GroupEventFeedbackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GroupEventFeedback
-        fields = '__all__'
+# class AttendanceSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Attendance
+#         fields = '__all__'
 
-class ImprovedWeaknessesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ImprovedWeaknesses
-        fields = ['weakness_type']
+# class MeetingFeedbackSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MeetingFeedback
+#         fields = '__all__'
 
-class GeneralFeedbackSerializer(serializers.ModelSerializer):
-    weaknesses = ImprovedWeaknessesSerializer(many=True)
+# class GroupEventFeedbackSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = GroupEventFeedback
+#         fields = '__all__'
 
-    class Meta:
-        model = GeneralFeedback
-        fields = ['rating', 'positives', 'negatives', 'giver', 'creation_datetime', 'improved_weaknesses']
+# class ImprovedWeaknessesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ImprovedWeaknesses
+#         fields = ['weakness_type']
 
-    def create(self, validated_data):
-        weakness_data = validated_data.pop('improved_weaknesses')
-        feedback = GeneralFeedback.objects.create(**validated_data)
-        weakness_list = [ImprovedWeaknesses(weakness_type=item['weakness_type'], feedback=feedback) for item in weakness_data]
-        ImprovedWeaknesses.objects.bulk_create(weakness_list)
-        return feedback
+# class GeneralFeedbackSerializer(serializers.ModelSerializer):
+#     weaknesses = ImprovedWeaknessesSerializer(many=True)
 
-class SystemFeedbackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SystemFeedback
-        fields = '__all__'
+#     class Meta:
+#         model = GeneralFeedback
+#         fields = ['rating', 'positives', 'negatives', 'giver', 'creation_datetime', 'improved_weaknesses']
 
-class MilestoneSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Milestone
-        fields = '__all__'
+#     def create(self, validated_data):
+#         weakness_data = validated_data.pop('improved_weaknesses')
+#         feedback = GeneralFeedback.objects.create(**validated_data)
+#         weakness_list = [ImprovedWeaknesses(weakness_type=item['weakness_type'], feedback=feedback) for item in weakness_data]
+#         ImprovedWeaknesses.objects.bulk_create(weakness_list)
+#         return feedback
 
-class PasswordResetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PasswordRest
-        fields = '__all__'
+# class SystemFeedbackSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = SystemFeedback
+#         fields = '__all__'
 
-class PairingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pairing
-        fields = '__all__'
+# class MilestoneSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Milestone
+#         fields = '__all__'
 
-class MeetingProposalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MeetingProposal
-        fields = '__all__'
+# class PasswordResetSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = PasswordRest
+#         fields = '__all__'
 
-class MeetingRequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MeetingRequest
-        fields = '__all__'
+# class PairingSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Pairing
+#         fields = '__all__'
+
+# class MeetingProposalSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MeetingProposal
+#         fields = '__all__'
+
+# class MeetingRequestSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MeetingRequest
+#         fields = '__all__'
--- mentorprise/backend_dev/mentorprise/app/urls.py
+++ mentorprise/backend_dev/mentorprise/app/urls.py
@@ -2,83 +2,91 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    ############
-    ### User ###
-    ############
-    path('users/profiles/', views.user_profile_list, name='user-profiles-list'),
-    path('users/profile/<str:user_id>/',
-         views.user_profile, name='user-profile'),
-    path('users/register/', views.user_register, name='user-register'),
-    path('users/login/', views.user_login, name='user-login'),
-    path('users/delete/<str:user_id>/', views.user_delete, name='user-delete'),
-    path('users/email/<str:user_id>/', views.user_email, name='user-email'),
-    path('users/password/<str:user_id>/',
-         views.user_password, name='user-password'),
-
-    ##############
-    ### Topics ###
-    ##############
-    path('topics/', views.topics_list, name='topics-list'),
-    path('topics/<str:sw_type>', views.topic_delete, name='topic-delete'),
-
-    #####################
-    ### Notifications ###
-    #####################
-    path('notifications/<str:user_id>/',
-         views.notifications_list, name='notifications-list'),
-
-    ################
-    ### Feedback ###
-    ################
-    # TODO: There might be a neater way to do this than multiple slugs
-    path('feedback/meetings/<str:user_id>/<str:meeting_id>/',
-         views.feedback_meeting, name='feedback-meeting'),
-    path('feedback/mentor/<str:user_id>/',
-         views.feedback_mentor, name='feedback-mentor'),
-    path('feedback/system/<str:user_id>/',
-         views.feedback_system, name='feedback-system'),
-
-    ####################
-    ### Group events ###
-    ####################
-    path('group_events/<str:user_id>/',
-         views.group_events_list, name='group-events'),
-    path('group_events/make/<str:user_id>/',
-         views.group_events_make, name='group-events-make'),
-    path('group_events/join/<str:user_id>/',
-         views.group_events_join, name='group-events-join'),
-
-    #######################
-    ### Plans of action ###
-    #######################
-    path('plans_of_action/<str:user_id>/',
-         views.plan_of_action_list, name='plans-of-action'),
-    path('plans_of_action/milestones/<str:user_id>/<str:milestone_id>/',
-         views.plan_of_action_milestone, name='plans-of-action-milestones'),
-
-    ################
-    ### Meetings ###
-    ################
-    path('meetings/<str:user_id>/', views.meetings_list, name='meetings-list'),
-    path('meetings/request/<str:user_id>/',
-         views.meetings_request, name='meetings-request'),
-    path('meetings/propose/<str:user_id>/',
-         views.meetings_propose, name='meetings-propose'),
-    path('meetings/accept/<str:user_id>/',
-         views.meetings_accept, name='meetings-accept'),
-
-
-    #################
-    ### Mentoring ###
-    #################
-    path('mentoring/mentees/<str:user_id>/',
-         views.mentoring_mentees_list, name='mentoring-mentees-list'),
-    path('mentoring/mentee/<str:user_id>/<str:mentee_id>/',
-         views.mentoring_mentee, name='mentoring-mentee'),
-    path('mentoring/mentor/<str:user_id>/',
-         views.mentoring_mentor, name='mentoring-mentor'),
-    path('mentoring/proposed_mentors/<str:user_id>/',
-         views.mentoring_proposed_mentors, name='mentoring-proposed-mentors'),
-    path('mentoring/potential_mentees/<str:user_id>/',
-         views.mentoring_potential_mentees_list, name='mentoring-potential-mentees-list'),
+    path('users/profiles/', views.user_profile_list),
+    path('users/register/', views.user_register),
+    path('users/login/', views.user_login),
+    path('users/profile/', views.user_profile),
 ]
+
+
+# urlpatterns = [
+#     ############
+#     ### User ###
+#     ############
+#     path('users/profiles/', views.user_profile_list, name='user-profiles-list'),
+#     path('users/profile/<str:user_id>/',
+#          views.user_profile, name='user-profile'),
+#     path('users/register/', views.user_register, name='user-register'),
+#     path('users/login/', views.user_login, name='user-login'),
+#     path('users/delete/<str:user_id>/', views.user_delete, name='user-delete'),
+#     path('users/email/<str:user_id>/', views.user_email, name='user-email'),
+#     path('users/password/<str:user_id>/',
+#          views.user_password, name='user-password'),
+
+#     ##############
+#     ### Topics ###
+#     ##############
+#     path('topics/', views.topics_list, name='topics-list'),
+#     path('topics/<str:sw_type>', views.topic_delete, name='topic-delete'),
+
+#     #####################
+#     ### Notifications ###
+#     #####################
+#     path('notifications/<str:user_id>/',
+#          views.notifications_list, name='notifications-list'),
+
+#     ################
+#     ### Feedback ###
+#     ################
+#     # TODO: There might be a neater way to do this than multiple slugs
+#     path('feedback/meetings/<str:user_id>/<str:meeting_id>/',
+#          views.feedback_meeting, name='feedback-meeting'),
+#     path('feedback/mentor/<str:user_id>/',
+#          views.feedback_mentor, name='feedback-mentor'),
+#     path('feedback/system/<str:user_id>/',
+#          views.feedback_system, name='feedback-system'),
+
+#     ####################
+#     ### Group events ###
+#     ####################
+#     path('group_events/<str:user_id>/',
+#          views.group_events_list, name='group-events'),
+#     path('group_events/make/<str:user_id>/',
+#          views.group_events_make, name='group-events-make'),
+#     path('group_events/join/<str:user_id>/',
+#          views.group_events_join, name='group-events-join'),
+
+#     #######################
+#     ### Plans of action ###
+#     #######################
+#     path('plans_of_action/<str:user_id>/',
+#          views.plan_of_action_list, name='plans-of-action'),
+#     path('plans_of_action/milestones/<str:user_id>/<str:milestone_id>/',
+#          views.plan_of_action_milestone, name='plans-of-action-milestones'),
+
+#     ################
+#     ### Meetings ###
+#     ################
+#     path('meetings/<str:user_id>/', views.meetings_list, name='meetings-list'),
+#     path('meetings/request/<str:user_id>/',
+#          views.meetings_request, name='meetings-request'),
+#     path('meetings/propose/<str:user_id>/',
+#          views.meetings_propose, name='meetings-propose'),
+#     path('meetings/accept/<str:user_id>/',
+#          views.meetings_accept, name='meetings-accept'),
+
+
+#     #################
+#     ### Mentoring ###
+#     #################
+#     path('mentoring/mentees/<str:user_id>/',
+#          views.mentoring_mentees_list, name='mentoring-mentees-list'),
+#     path('mentoring/mentee/<str:user_id>/<str:mentee_id>/',
+#          views.mentoring_mentee, name='mentoring-mentee'),
+#     path('mentoring/mentor/<str:user_id>/',
+#          views.mentoring_mentor, name='mentoring-mentor'),
+#     path('mentoring/proposed_mentors/<str:user_id>/',
+#          views.mentoring_proposed_mentors, name='mentoring-proposed-mentors'),
+#     path('mentoring/potential_mentees/<str:user_id>/',
+#          views.mentoring_potential_mentees_list, name='mentoring-potential-mentees-list'),
+# ]
