5aefc1da582954e2b8010265ba9977f05f75c136
EdmundGoodman <egoodman3141@gmail.com>
2022-03-13 00:54:31 +0000

    Tried to get codecov working

--- mentorprise/docker/frontend/vue.config.js
+++ mentorprise/docker/frontend/vue.config.js
@@ -2,10 +2,6 @@ module.exports = {
   devServer: {
     proxy: {
       "^/backend/*": {
-        target: "http://localhost:8000",
-        secure: false,
-        changeOrigin: true,
-        logLevel: "debug",
         pathRewrite: { "^/backend": "" },
       },
     },
